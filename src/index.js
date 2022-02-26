e503d879a099f797ffc749752445cdfe625e338f
minirachel <rachel.a.levinson@gmail.com>
2022-02-26 14:01:35 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newItem = document.getElementById("new-task-description").value;
+  const taskUl = document.getElementById("tasks");
+
+  const newTaskUl = document.getElementById("tasks");
+
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+}
+
+
+
+
+
+
+
+
